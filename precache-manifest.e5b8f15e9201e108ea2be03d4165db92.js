c45996a7e387843cfd2f215a750cc37f85aa1074
allockicmoi <allockicmoi@gmail.com>
2020-03-01 20:09:16 -0500

    Updates

---
+++ precache-manifest.e5b8f15e9201e108ea2be03d4165db92.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "43f673eac5958e6a97f391006c04de12",
+    "url": "/algo-display/index.html"
+  },
+  {
+    "revision": "7614a2bbabb96ce5c82d",
+    "url": "/algo-display/static/css/2.d58fcd6a.chunk.css"
+  },
+  {
+    "revision": "1b731441313310130975",
+    "url": "/algo-display/static/css/main.cb910570.chunk.css"
+  },
+  {
+    "revision": "7614a2bbabb96ce5c82d",
+    "url": "/algo-display/static/js/2.a9ba1a22.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/algo-display/static/js/2.a9ba1a22.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b731441313310130975",
+    "url": "/algo-display/static/js/main.0a0c3fd5.chunk.js"
+  },
+  {
+    "revision": "2c0df24fe894cf6ccedc",
+    "url": "/algo-display/static/js/runtime-main.e0a28a96.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/algo-display/static/media/logo.5d5d9eef.svg"
+  }
+]);
